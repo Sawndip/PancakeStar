19842831cd4c59bb57bc26918399c58b2e613f37
Sergio Troncoso <stroncod@gmail.com>
2019-02-09 17:53:18 -0300

    fix bug on update

--- main.cpp
+++ main.cpp
@@ -20,20 +20,22 @@
 #include <boost/unordered_map.hpp>
 #include <stdexcept>
 #include <chrono>
+#include <functional>
 
 
 using namespace std;
 using namespace boost::heap;
 
 struct State{
-	int id;
+	size_t id;
 	vector<int> list;
 	float g;
     float h;
     float f;
     boost::any handler_open;
-	int n = list.size();
-	State(int _id, vector<int> _list, double j, double h_, double k) : id(_id) ,list(_list),g(j),h(h_),f(k) {}
+	//int n = list.size();
+	State* parent;
+	State( size_t _id,vector<int> _list, double j, double h_, double k, State* f_) :  id(_id),list(_list),g(j),h(h_),f(k), parent(f_) {}
 	~State(){}
 
 	State & operator=(const State & _state){
@@ -42,11 +44,12 @@ struct State{
 		this->list = _state.list;
 		this->g = _state.g;
 		this->h = _state.h;
-		this->f = _state.f;
+		this->f = _state.f;//(g+h)*1000000 -g
 	};
 
 	bool operator==(const State & _state) const{
-		return (this==&_state);
+		//return (this==&_state);
+		return this->id==_state.id;
 	};
 
 };
@@ -58,22 +61,35 @@ struct compare_states {
     }
 };
 
-
-struct hash_states<State>
-{
-    std::size_t operator()(const State& c) const
-    {
-        std::size_t result = c.list.size();
-        for(unsigned i = 0; i < c.list.size(); ++i) {
-        	boost::hash_combine(result, c.list[i]+i);
+struct hash_states{
+    std::size_t operator()(const State* & c) const {
+        std::size_t result = c->list.size();
+        for(unsigned i = 0; i < c->list.size(); ++i) {
+        	boost::hash_combine(result, c->list[i]+i);
         }
         return result;
     }
 };
 
+/*
+struct equal_states {
+		bool operator() (Nodo const* t1, Nodo const* t2) const{
+    	//std::cout << "Equal called"<<std::endl;
+    	//return t1->id==t2->id && t1->x==t2->x && t1->y==t2->y;
+		return t1->id==t2->id;
+  	}
+
+}
+*/
+
+
+
 
 typedef fibonacci_heap<State*,compare<compare_states> >::handle_type open_handle;
-boost::unordered_map<int, open_handle> node_map;
+boost::unordered_map<State*, open_handle> open_map;
+boost::unordered_map<size_t, State*> node_map; //change to hash,int
+
+//boost::unordered_set<State*,hash_states> state_set;
 
 fibonacci_heap<State*, boost::heap::compare<compare_states> > open;
 boost::container::vector<State*> closed;
@@ -83,11 +99,34 @@ set<vector<int>> goal_set;
 int generated_nodes;
 int expanded_nodes;
 vector<int> goal_list;
-state *solution;
+State *solution;
+
+vector<double> average_time;
+vector<int> average_sol;
+vector<int> average_exp_n;
+vector<int> average_gen_n;
+
+
+
+size_t gen_id(State* state ){ 
+    std::size_t result = state->list.size();
+    for(unsigned i = 0; i < state->list.size(); ++i) {
+        boost::hash_combine(result, state->list[i]+i);
+    }
+    return result;
+} 
 
 void printList(State* currentState ){
 	for (int& x: currentState->list) cout << ' ' << x;
     std::cout << '\n';
+}
+
+void printMap(){
+	auto it = node_map.begin();
+	for(unsigned i = 1; i < 5; ++i) {
+		cout<<"nodo id:"<<it->first<<" h:"<<it->second->h<<endl;
+		advance(it,i);
+	}
 }
 
 	
@@ -149,28 +188,51 @@ vector<int> flip(vector<int> s1, int i) {
 void getSuccessors(State* _current, double w){
 	
 	for(size_t i = 2; i < _current->list.size(); ++i) {
-		node_id++;
+
 		generated_nodes++;       
-		state* succ = new state(node_id,flip(_current->list,i),0,0,0);
+		State* succ = new State(0,flip(_current->list,i),0,0,0,_current);
+		succ->id = gen_id(succ);
 		succ->g = _current->g +1;              
-		succ->h = hgap(succ->list)*w;
-		succ->f = succ->h+1;//+succ->g; 
+		
+		
+
+		//check if already is generated
+		auto in_map = node_map.find(succ->id);
+		if(in_map != node_map.end()){
+			//in_map->second = succ;
+			succ->h = in_map->second->h;		
+			
+		} else{		
+			succ->h = hgap(succ->list)*w;
+			
+			node_map.emplace(succ->id,succ);
+
+		}
+		succ->f = (succ->h+ succ->g)*100000 - succ->g; 
 
 		//check if is open 
 		
-		auto node_find = node_map.find(node_id);
+		auto node_find = open_map.find(succ);
 		auto state_f = find(closed.begin(),closed.end(),succ);
-		if(node_find != node_map.end()){ //if in the open list
-			open.update(node_find->second,succ);
+
+		if(node_find != open_map.end()){ //if in the open list
+
+			open.update(node_find->second);
+			
 		}
 
 		else if(state_f != closed.end()){ //in closed?
+
 			closed.erase(state_f);
-			open.push(succ);
+			auto prt_open = open.push(succ);
+			open_map.emplace(succ,prt_open);
+			
 		}
 		else {
+
 			auto prt_open = open.push(succ);
-			node_map.emplace(node_id,prt_open);
+			open_map.emplace(succ,prt_open);
+			
 
 		}
 	}
@@ -190,49 +252,120 @@ vector<int> createProblem (int n, int seed){
 	int back = v0.back();
 	v0.pop_back();
 
-
-
 	shuffle(v0.begin(), v0.end(), e);
 
 	v0.push_back(back);
-
-
 	return v0;
 }
 
 
 
-int aStar(vector<int> s0, double w) {
+State* aStar(vector<int> s0, double w, int lookahead) {
+	cout<<"Doing A* search"<<endl;
+	int iter = 0;
+	State* current;
 
-	State *initial_state = new state(node_id,s0,0,hgap(s0),0);
+	State *initial_state = new State(0,s0,0,hgap(s0),0,NULL);
+	initial_state->id = gen_id(initial_state);
+	//check if is in the transposition map
+	auto it_node = node_map.find(initial_state->id);
+	if ( it_node != node_map.end() ){
+		initial_state->h = it_node->second->h;
+	} else {
+		node_map.emplace(initial_state->id,initial_state);
+	}
 
 	auto prt_open = open.push(initial_state);
-	node_map.emplace(node_id,prt_open);
+	open_map.emplace(initial_state,prt_open);
 	
 	
-	while(!open.empty()){
+	
+	while(!open.empty()  && iter < lookahead ){
 		
 		//count_close_states++;
-		State* current = open.top();
+		current = open.top();
 
 		closed.push_back(current);
 
 		open.pop();
-		expanded_nodes++;  
-		//cout<<"Current state:"<<endl;
-		//printList(current);
-		//IF ARRAY IS DONE
-		if(checkForGoal(current->list)){
-			solution = current ; 
-			return 1;
+		//update open map
+		open_map.erase(open_map.find(current));
+		expanded_nodes++; 
+
+		//printList(current); 
+
+		if( checkForGoal(current->list) ) {
+			//solution = current ; 
+			return current;
 		}
 		getSuccessors(current,w);
-
+		iter++;
 	}
-	return -1;
+	return current;
+
+}
+boost::container::vector<State*>  children(State* s) {
+	boost::container::vector<State*> childrens;
+
+	//checking in closed list 
+	for (auto &state : closed) {
+		
+		if(state->parent == s) {
+			childrens.push_back(state);
+		}
+	}
+	//checking in open list 
+	for (auto &state : open) {
+		if(state->parent == s) {
+			childrens.push_back(state);
+		}
+	}
+	//imprimir el mejor de la open
+	return childrens;
 
 }
 
+
+int update_h(State* s){
+	
+
+	//actualizar mapa, no las estructuras
+	boost::container::vector<State*> childrens = children(s);
+	auto node_find = open_map.find(s);
+	if( node_find != open_map.end() ) {
+
+		return s->h;
+	}
+	int min = numeric_limits<unsigned short int>::max();
+	
+	if ( childrens.empty() ){ 
+		//cout<<"1"<<endl;
+		return min;
+	} else {
+		//cout<<"2"<<endl;
+		for (auto &state : childrens ){
+			int h = update_h(state);
+			//cout<<"4"<<endl;
+
+			if(min > 1+h){
+				min = 1+h;
+			}
+		}
+		auto it_node = node_map.find(s->id);
+		if ( it_node != node_map.end() ){
+			//cout<<"3"<<endl;
+			it_node->second->h = min;
+		} else {
+			cout<<"State not found"<<endl;
+		}
+
+	}
+
+	return min;
+
+}
+
+/*
 void update_h (){
 	for(unsigned i = 0; i <closed.size() ; ++i) {
 		closed[i]->h = numeric_limits<unsigned short int>::max();
@@ -242,21 +375,90 @@ void update_h (){
 	while(!open.empty()) {
 		State* open_b = open.top();
 
-		for(size_t i = 2; i < open_b->n; ++i) {   
-			cout<<"PCHDISHFCSDIJFCISD"<<endl;    
-			State* succ = new state(node_id,flip(open_b->list,i),open_b->g +1,hgap(succ->list),0);
-			succ->f = succ->h;
+		for(size_t i = 2; i < open_b->list.size(); ++i) {   
+
+			State* succ = new State(0,flip(open_b->list,i),0,0,0,open_b);
+			succ->id = gen_id(succ);
+			
+			auto in_map = node_map.find(succ->id);
+			if(in_map != node_map.end()){
+				succ = in_map->second;
+				succ->f = succ->h;
+
+
+			} else {
+				succ->f = succ->h;
+				succ->g = open_b->g +1;
+				succ->h = hgap(succ->list);
+			}
+			
 			auto succ_f = find(closed.begin(),closed.end(),succ); 
 			if(succ_f != closed.end()){
-				if((*succ_f)->h > open_b->h +succ->g /*Es el g? */ ){
+				cout<<"Entro aqui uwu?"<<endl;
+				if((*succ_f)->h > open_b->h + 1/*succ->g )
+				{
 					(*succ_f)->h = open_b->h + succ->g;
-					open.push(succ);
+					auto prt_open = open.push(succ);
+					open_map.emplace(succ,prt_open);
+					cout<<"Entro aqui owo?"<<endl;
 				} 
 			}		
 
 		}
 	}
 }
+
+*/
+
+
+int driver(vector<int> myvector, int lookahead) {
+	
+	int iter = 0;
+
+	//verificar si llego al objetivo
+	
+	while(1) {
+		State *prop_sol = aStar(myvector,1.5,lookahead);//numero + grande que el numero de operaciones optimas
+		cout<<"nodos expandidos dsps astar: "<<expanded_nodes<<endl;
+		//get start node
+		
+		//printList(prop_sol);
+
+		if (checkForGoal(prop_sol->list)) {
+			solution = prop_sol; 
+			return 1; 
+
+		}
+		if(iter > 100000) {
+			return -1;	
+		}
+
+
+		State* first = closed[0];
+		cout<<"h antes update: "<<first->h<<endl;
+		auto it_node= node_map.find(first->id);
+		if ( it_node != node_map.end() ){
+			cout<<"Doing update_heuristic"<<endl;
+			it_node->second->h = update_h(first); 
+		} else {
+			cout<<"State not found"<<endl;
+			return -1;
+		}
+		cout<<"First h: "<<it_node->second->h<<endl;
+		cin.get();
+		printMap();
+
+		iter++;
+		cout<<node_map.size()<<endl;
+		open.clear();
+		closed.clear();
+		open_map.clear();
+	}
+	
+}
+
+
+
 
 //TODO:
 //revisar si la heuristica convergio
@@ -269,10 +471,7 @@ int main(int argc, char const *argv[])
 {
 	int n_cases = atoi(argv[1]);
 	int j =0 ;
-	vector<double> average_time;
-	vector<int> average_sol;
-	vector<int> average_exp_n;
-	vector<int> average_gen_n;
+
 	double w = 1;
 	while(j < n_cases){
 		srand (4*(j+1));
@@ -280,7 +479,8 @@ int main(int argc, char const *argv[])
 		goal_set.insert(goal_list);
 		chrono::high_resolution_clock::time_point start = chrono::high_resolution_clock::now();
 
-		int resp = aStar(myvector,1.0);
+		int resp = driver(myvector,atoi(argv[3]));
+		node_map.clear();
 		//update_h();
 		//resp = aStar(myvector,1.0);
 		chrono::high_resolution_clock::time_point end = chrono::high_resolution_clock::now();
@@ -299,15 +499,17 @@ int main(int argc, char const *argv[])
 			average_sol.push_back(solution->g);
 			average_exp_n.push_back(expanded_nodes);
 			average_gen_n.push_back(generated_nodes);
+
+
 		} else {
+
 			cout<<"No solution find for the problem, NO pancakes for you!"<<endl;
 		}
-
-		goal_set.clear();
-		open.clear();
-		closed.clear();
+		
 		goal_list.clear();
-		node_map.clear();
+		goal_set.clear();
+
+
 		expanded_nodes = 0;
 		generated_nodes = 0;
 
